7f94c2c5f2e51cd7fa32d3aaa0c95b9391e89029
Matthew Ellis <mbellistx@gmail.com>
2021-04-16 15:41:30 -0500

    api scaffolding done

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,11 @@
 // build your `/api/resources` router here
+const router = require('express').Router()
+
+const { getResources } = require('./model')
+
+router.get('/', async (_, res) => {
+  const resources = await getResources()
+  res.status(200).json(resources)
+})
+
+module.exports = router
