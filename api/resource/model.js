8501400d5561861f677c26533a117fd974291b66
Matthew Ellis <mbellistx@gmail.com>
2021-04-16 15:57:07 -0500

    recourse endpoints done

--- api/resource/model.js
+++ api/resource/model.js
@@ -3,8 +3,21 @@ const db = require('../../data/dbConfig.js')
 
 module.exports = {
   getResources,
+  getResourceById,
+  insert,
 }
 
 function getResources(){
   return db('resources')
+}
+
+function getResourceById(resource_id){
+  return db('resources')
+    .where({resource_id})
+    .first()
+}
+
+async function insert(resource){
+  const [id] = await db('resources').insert(resource)
+  return getResourceById(id)
 }
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,11 +1,30 @@
 // build your `/api/resources` router here
 const router = require('express').Router()
 
-const { getResources } = require('./model')
+const resources = require('./model')
 
 router.get('/', async (_, res) => {
-  const resources = await getResources()
-  res.status(200).json(resources)
+  const allResources = await resources.getResources()
+  res.status(200).json(allResources)
+})
+
+router.post('/', (req, res, next) => {
+  const {
+    resource_name,
+    resource_description
+  } = req.body
+
+  if(typeof(resource_name) === 'string'){
+    resources.insert({ resource_name, resource_description})
+      .then( newResource => {
+        res.status(201).json(newResource)
+      })
+      .catch( err => {
+        res.status(400).json('resource_name must be unique')
+      })
+  } else {
+    res.status(400).json('resource_name required string')
+  }
 })
 
 module.exports = router
